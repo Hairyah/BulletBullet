58148d5b5a780c1955d4aff4464336fc320b5e23
Jothecat16 <antoinevermesse0@gmail.com>
2023-07-08 03:15:34 +0200

    Initial Commit

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Camera cam;
+
+    private float mousePosX;
+    private float mousePosY;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        //Initialisation des inputs
+        mousePosX = Input.GetAxis("Mouse X");
+        mousePosY = Input.GetAxis("Mouse Y");
+
+        LimitMouseSpeed();
+   
+        transform.Rotate(new Vector3(mousePosY, mousePosX, 0), Space.Self);
+        Debug.Log(new Vector2(mousePosX, mousePosY));
+
+        //Déplacements
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
+    }
+
+    private void LimitMouseSpeed()
+    {
+        if (mousePosX > 0.5)
+            mousePosX = 1;
+        if (mousePosX < -1)
+            mousePosX = -1;
+
+        if (mousePosY > 1)
+            mousePosY = 1;
+        if (mousePosY < -1)
+            mousePosY = -1;
+    }
+}
